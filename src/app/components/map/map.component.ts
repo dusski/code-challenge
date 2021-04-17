f090c16a7a9674c97f5c8cc53d1a63466775dde4
Dusan Skiljevic <skiljevic.dusan@gmail.com>
2021-04-17 15:59:46 +0200

    Added map element and hight and width properties

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  width: number;
+  height: number;
+
   constructor() { }
 
   ngOnInit(): void {
