9301478e9b4f2f6f198715b05aabc1ce82ae619a
Dusan Skiljevic <skiljevic.dusan@gmail.com>
2021-04-17 19:04:47 +0200

    Extended map component with inputs for data

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+declare var H: any;
 
 @Component({
   selector: 'app-map',
@@ -7,12 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  width: number;
-  height: number;
+  @ViewChild("map")
+  public mapElement: ElementRef;
 
-  constructor() { }
+  @Input()
+  public latitude: any;
 
-  ngOnInit(): void {
+  @Input()
+  public longitude: any;
+
+  @Input()
+  public width: any;
+
+  @Input()
+  public height: any;
+
+  private platform: any;
+  private map: any;
+  private ui: any;
+  private search: any;
+
+  public constructor() { }
+
+  public ngOnInit() {
+    this.platform = new H.service.Platform({
+      "app_id": environment.appId,
+      "apikey": environment.apiKey
+    });
+
+    this.search = new H.places.Search(this.platform.getPlacesService());
+  }
+
+  public ngAfterViewInit() {
+    let defaultLayers = this.platform.createDefaultLayers();
+    this.map = new H.Map(
+      this.mapElement.nativeElement,
+      defaultLayers.vector.normal.map,
+      {
+        zoom: 10,
+        center: {
+          lat: this.latitude,
+          lng: this.longitude
+        }
+      }
+    );
+
+    let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
+    this.ui = H.ui.UI.createDefault(this.map, defaultLayers);
   }
 
 }
