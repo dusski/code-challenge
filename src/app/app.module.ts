418e0aaf534246da69af434f0f047ac36909fab0
Dusan Skiljevic <skiljevic.dusan@gmail.com>
2021-04-17 15:55:26 +0200

    Added separate map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// components
 import { AppComponent } from './app.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
