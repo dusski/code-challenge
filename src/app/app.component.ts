2d696f6254116cdd45a950ada5c3a4ac93245da0
Dusan Skiljevic <skiljevic.dusan@gmail.com>
2021-04-17 19:08:23 +0200

    Removed previous code from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
-declare var H: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,29 +7,4 @@ declare var H: any;
 })
 export class AppComponent {
   title = 'map-challenge';
-
-  private platform: any;
-
-  @ViewChild("map")
-  public mapElement: ElementRef;
-
-  public constructor() {
-    this.platform = new H.service.Platform({
-      "apikey": "API_KEY"
-    });
-  }
-
-  public ngOnInit() { }
-
-  public ngAfterViewInit() {
-    let defaultLayers = this.platform.createDefaultLayers();
-    let map = new H.Map(
-      this.mapElement.nativeElement,
-      defaultLayers.vector.normal.map,
-      {
-        zoom: 10,
-        center: { lat: 37.7397, lng: -121.4252 }
-      }
-    );
-  }
 }
