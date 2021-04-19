be6e2e87c531abebcfa81e7c4c997db51d4eb1f1
d.skiljevic <d.skiljevic@futureforward.rs>
2021-04-19 10:10:39 +0200

    Added new booking page and implemented forms

--- src/app/pages/booking-page/booking-page.component.ts
+++ src/app/pages/booking-page/booking-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-booking-page',
@@ -7,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingPageComponent implements OnInit {
 
-  constructor() { }
+  hotel: string = '';
+  bookingForm: any;
+  private _destruction$: Subject<any> = new Subject<any>();
+
+  get firstName(): FormControl {
+    return this.bookingForm.get('firstName');
+  }
+  get lastName(): FormControl {
+    return this.bookingForm.get('lastName');
+  }
+  get dateFrom(): FormControl {
+    return this.bookingForm.get('dateFrom');
+  }
+  get dateTo(): FormControl {
+    return this.bookingForm.get('dateTo');
+  }
+
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _formBuilder: FormBuilder
+  ) {
+
+    this.bookingForm = this._formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      dateFrom: ['', Validators.required],
+      dateTo: ['', Validators.required],
+      comments: [''],
+    });
+
+  }
 
   ngOnInit(): void {
+    this._route.params
+      .pipe(
+        takeUntil(this._destruction$)
+      )
+      .subscribe(
+        (params: Params) => {
+          this.hotel = params.place;
+        }
+      );
+  }
+
+  onSubmit() {
+    console.log(this.bookingForm.value);
+    this._router.navigate(['/book-success']);
+  }
+
+  onCancel() {
+    this._router.navigate(['/']);
   }
 
 }
