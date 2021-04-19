b7ea02a614c0c1f3d0fedbd6f89b18ea7e689c26
Dusan Skiljevic <skiljevic.dusan@gmail.com>
2021-04-19 08:07:05 +0200

    Updated map to be set to the users curent position

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { DataService } from 'src/app/services/location/data.service';
+import { first } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 declare var H: any;
@@ -10,8 +12,12 @@ declare var H: any;
 })
 export class MapComponent implements OnInit {
 
-  @ViewChild("map")
-  public mapElement: ElementRef;
+  private _platform: any;
+  private _map: any;
+  private _ui: any;
+  private _search: any;
+
+  @ViewChild("map") public mapElement: ElementRef;
 
   @Input()
   public latitude: any;
@@ -25,12 +31,9 @@ export class MapComponent implements OnInit {
   @Input()
   public height: any;
 
-  private _platform: any;
-  private _map: any;
-  private _ui: any;
-  private _search: any;
-
-  public constructor() { }
+  public constructor(
+    private _dataService: DataService
+  ) { }
 
   public ngOnInit() {
     this._platform = new H.service.Platform({
@@ -40,23 +43,30 @@ export class MapComponent implements OnInit {
   }
 
   public ngAfterViewInit() {
-    let defaultLayers = this._platform.createDefaultLayers();
-    this._map = new H.Map(
-      this.mapElement.nativeElement,
-      defaultLayers.vector.normal.map,
-      {
-        zoom: 13,
-        center: {
-          lat: this.latitude,
-          lng: this.longitude
-        }
-      }
-    );
+    let defaultLayers = this._platform?.createDefaultLayers();
+    this._map = new H.Map(this.mapElement.nativeElement, defaultLayers.vector.normal.map);
 
     let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this._map));
-    this._ui = H.ui.UI.createDefault(this._map, defaultLayers);
 
-    this._search = new H.places.Search(this._platform.getPlacesService());
+    window.navigator.geolocation.getCurrentPosition(
+      (position: GeolocationPosition) => {
+        const { latitude: lat, longitude: lng } = position.coords;
+        this._dataService.currentLocation = [lat, lng];
+
+        this._map?.setCenter({ lat, lng });
+        this._map?.setZoom(13, true);
+
+        this.queryData();
+      }
+    );
+  }
+
+  queryData() {
+    this._dataService.queryLocation('hotels')
+      .pipe(first())
+      .subscribe(
+        (response: any) => {},
+      );
   }
 
 }
